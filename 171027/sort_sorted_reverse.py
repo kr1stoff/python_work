381835aa5825e868ecfcb0f7cd49a29d4a7a646e
Kristoff <33119892+kr1stoff@users.noreply.github.com>
2017-10-27 15:59:42 +0800

    python_work

---
+++ 171027/sort_sorted_reverse.py
@@ -0,0 +1,15 @@
+favorite_city = ['shanghai','chongqing','changsha','chengdu','wuhan']
+print (favorite_city)
+print (sorted(favorite_city))
+print (favorite_city)
+
+favorite_city.reverse()
+print(favorite_city)
+favorite_city.reverse()
+print (favorite_city)
+
+favorite_city.sort()
+print (favorite_city)
+
+favorite_city.sort(reverse = True)
+print (favorite_city)
