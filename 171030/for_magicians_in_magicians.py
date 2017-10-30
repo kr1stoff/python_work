18fdff8b3247b0a11ba59680c3e99737a25aa921
Kristoff <33119892+kr1stoff@users.noreply.github.com>
2017-10-30 14:10:20 +0800

    del

--- 171030/for_magicians_in_magicians.py
+++
@@ -1,4 +0,0 @@
-magicians = ['alice','david','carolina']
-for magician in magicians:
-    print (magician.title() + ",you are a great trick!")
-print ("I can't wait to see your next trick" + magician.title() + ".\n")
