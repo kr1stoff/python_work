4d8bdd910750b77a291ca08c753c3d393c30d750
kr1stoff <706060951@qq.com>
2017-11-03 22:21:39 +0800

    down

--- 201710/171030/for_magicians_in_magicians.py
+++ 201710/171030/for_magicians_in_magicians.py
@@ -1,3 +1,6 @@
 magicians = ['alice','david','carolina']
 for magician in magicians:
     print (magician)
+magicians[0]='sb'
+for magician in magicians:
+    print(magician)
---
+++ 201711/1102/dimensions_2.py
@@ -0,0 +1,6 @@
+dimensions = (200,50)
+print ("Original dimensions:")
+for dimension in dimensions:
+    print (dimension)
+
+print(dimensions)
