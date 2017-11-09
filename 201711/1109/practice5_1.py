50c3fe3723c0bca5ce97321097bbd00273c7d32d
kr1stoff <706060951@qq.com>
2017-11-10 01:14:08 +0800

    update

---
+++ 201711/1109/practice5_1.py
@@ -0,0 +1,34 @@
+car = 'subaru'
+print("is car == 'subaru'? I predict Ture.")
+print(car == 'subaru')
+
+print("Is car == 'audi'? I predict False.")
+print(car == 'audi')
+
+pajamas = 'grey'
+print("\nIs pajamas == 'grey'? I predict True.")
+print(pajamas == 'grey')
+
+print("Is pajamas == 'whtie'? I predict False.")
+print(pajamas == 'white')
+
+cup = 'medium'
+print("\nIs cup == 'medium'? I predict True.")
+print(cup == 'medium')
+
+print("Is cup == 'small'? I predict False.")
+print(cup == 'small')
+
+status = 'sleepy'
+print("\nIs status == 'sleepy'? I predict True.")
+print(status == 'sleepy')
+
+print("Is status == 'excited'? I predict False.")
+print(status == 'excited')
+
+scissor = '12cm'
+print("\nIs scissor == '12cm'? I predict True.")
+print(scissor == '12cm')
+
+print("Is scissor == '5cm'? I p5edict False.")
+print(scissor == '5cm')
