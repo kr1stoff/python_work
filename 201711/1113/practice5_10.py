45b3fc11565846d586697f5004fce58bbc59e630
Kristoff <706060951@qq.com>
2017-11-13 18:20:10 +0800

    update

---
+++ 201711/1113/practice5_10.py
@@ -0,0 +1,10 @@
+current_users = ['kristoff','xfmeng','zdpan','silin','byu','zxzheng']
+new_users = ['kristoff','jiuwo','yuge','junge','xfmeng']
+
+for new_user in new_users:
+    if new_user.lower() in current_users:
+        print("Need to input other user name.")
+    else:
+        print("User name not registed.")
+        
+        
---
+++ 201711/1113/practice5_9.py
@@ -0,0 +1,11 @@
+users = []
+
+if users:
+    for user in users:
+        if user == 'admin':
+            print("Hello admin,would you like to see a status report?")
+        else:
+            print("Hello " + user + ",thank you for logging in again.")
+        
+else:
+    print("we need to find some users!")
