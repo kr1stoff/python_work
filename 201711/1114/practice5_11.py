819e9a1db5fb1d4284228a49d63a7d548103cf0d
Kristoff <706060951@qq.com>
2017-11-14 17:39:54 +0800

    update

---
+++ 201711/1114/practice5_11.py
@@ -0,0 +1,10 @@
+numbers = range(1,10)
+
+for number in numbers:
+    if number == 1:
+        print("1st")
+    elif number == 2:
+        print("2nd")
+    else:
+        print(str(number) + "th")
+                                                                                
