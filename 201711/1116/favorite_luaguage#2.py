b2498cfd1e89354d5d3c826b1163df7b857b062a
Kristoff <706060951@qq.com>
2017-11-16 18:18:29 +0800

    update

---
+++ 201711/1116/favorite_luaguage#2.py
@@ -0,0 +1,10 @@
+favorite_luaguage = {
+    'jen': 'python',
+    'sarah': 'C',
+    'edward': 'ruby',
+    'phil': 'python',
+    }
+
+# 遍历这个字典
+for name,luaguage in favorite_luaguage.items():
+    print(name.title() + "'s favorite luaguage is ")
