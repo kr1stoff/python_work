54937d22eadc0240861ae175dac76b9dc4d218f1
Kristoff <706060951@qq.com>
2017-11-16 18:13:21 +0800

    update

---
+++ 201711/1116/practice#6_3.py
@@ -0,0 +1,12 @@
+python_dictionary = {
+    'upper': 'Change the string to uppercase',
+    'title': 'Show each word in a capital letter',
+    'str': 'Make numeric programming strings, avoid program errors',
+    'sort': 'Permanently modify list order, alphabetically',
+    'for': 'The statement iterates through each element of the list',
+}
+
+# upper指令含义
+print("Upper: " + python_dictionary['upper'] + ".")
+# sort指令含义
+print("\nSort: " + python_dictionary['sort'] + ".")
