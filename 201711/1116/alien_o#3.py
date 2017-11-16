9b32ea9de74587c05e7be7a129294e1f09513f84
Kristoff <706060951@qq.com>
2017-11-16 09:29:27 +0800

    update

--- 201711/1116/alien_o#3.py
+++ 201711/1116/alien_o#3.py
@@ -1,6 +1,9 @@
 alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
 print("Original x_position: " + str(alien_0['x_position']) + ".")
 
+# 外星人的移动速度变快
+alien_0['speed'] = 'fast'
+
 # 向右移动外星人
 # 根据外星人的当前速度决定其移动多远
 if alien_0['speed'] == 'slow':
