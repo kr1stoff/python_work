7a548d97088e5b012e023868d3d8b0a485c4f1ea
kr1stoff <706060951@qq.com>
2017-12-02 00:43:35 +0800

    1202 01:00 update

---
+++ 201712/practice#6-7.py
@@ -0,0 +1,25 @@
+Kristoff = {
+    'first_name': 'xf',
+    'last_name': 'meng',
+    'age': 24,
+    'city': 'DG',
+    }
+print(Kristoff)
+
+
+silin = {
+    'first name': 'yl',
+    'last name': 'li',
+    'city': 'GZ',
+    }
+
+jiuw = {
+    'first name': 'hr',
+    'last name': 'li',
+    'city':'harbin',
+}
+
+people = [jiuw,silin,Kristoff]
+
+for name in people:
+    print(name)
