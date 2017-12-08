3f3dc43a43fd70b8addc6e776e74fd8b234bba14
kr1stoff <706060951@qq.com>
2017-12-08 23:16:38 +0800

    1208 update

---
+++ 201712/greeter.py
@@ -0,0 +1,10 @@
+"""
+name = input("Please enter your name: ")
+print("Hello, " + name + " !")
+"""
+
+prompt = "If you tell us who you are, we can personaliza the message you see."
+prompt += "\nWhat is your name? "
+
+name = input(prompt)
+print("\nHello, " + name + "!")
