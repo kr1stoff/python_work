86fb4cdfc260b85df574b767788f66ad1c26cdb5
kr1stoff <706060951@qq.com>
2017-12-10 15:48:22 +0800

    practice#7 1210

---
+++ 201712/practice#7-2.py
@@ -0,0 +1,7 @@
+h_number = input("How many people are eating? ")
+h_number = int(h_number)
+
+if h_number > 8:
+    print("\nNo empty table")
+else:
+    print("\nHave empty table")
---
+++ 201712/practice#7-3.py
@@ -0,0 +1,7 @@
+number = input("Please enter a number! ")
+number = int(number)
+
+if number % 10 == 0:
+    print("\nThis number is multiple of 10.")
+else:
+    print("\nThis number isn't multiple of 10.")
