d8f09629d9dc2334c651d1547de55f8ea77f19d6
kr1stoff <706060951@qq.com>
2017-12-10 16:31:38 +0800

    remove&create

---
+++ 201712/parrot#while.py
@@ -0,0 +1,8 @@
+prompt = "\nTell me something, and I'll repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+message = ""
+while message != 'quit':
+    message = input(prompt)
+# 不repeat指令quit
+    if message != 'quit':
+        print(message)
