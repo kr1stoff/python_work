810bfd1f2bfaebe4e7ee00f106d85cfc28769733
kr1stoff <706060951@qq.com>
2017-12-17 00:36:03 +0800

    1216 update

---
+++ 201712/practice#7-5&7-6.py
@@ -0,0 +1,31 @@
+age_prompt = "\nI will determin the ticket price acconding to your age."
+age_prompt += "\nPlease enter 0 to quit!"
+age_prompt += "\nHow old are you? "
+
+"""  active
+active = True
+while active:
+    age = input(age_prompt)
+    age = int(age)
+    if age == 0:
+        active = False
+    elif  age < 3:
+        print('free')
+    elif age <= 12:
+        print('$10')
+    else:
+        print('$15')
+"""  
+
+""" while条件测试结束循环
+age = ""
+while  age != 0:
+    age = input(age_prompt)
+    age = int(age)
+    if age < 3:
+        print('free') 
+    elif age <= 12:
+        print('$10')
+    else:
+        print('$15')
+"""
